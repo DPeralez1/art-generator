6a39e585bd84133eb606d8e0e923644be4ebb9e7
David Peralez <davidperalez89@gmail.com>
2024-05-01 14:03:54 -0700

    "set up art generator"

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+
+const URL =
+  "https://collectionapi.metmuseum.org/public/collection/v1/objects/4539";
 
 function App() {
+  const [photo, setPhoto] = useState();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch(URL);
+      result.json().then((json) => {
+        console.log(json);
+        setPhoto(json.primaryImageSmall);
+      });
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Data Fetching in React</h1>
+      <div>Here is a photo:</div>
+      <img src={photo} />
     </div>
   );
 }
